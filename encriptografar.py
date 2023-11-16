f85240eec60dc7227d7924e6a7a2276974f03931
Yan Victor <106403574+TheYanVictor@users.noreply.github.com>
2023-11-15 22:35:05 -0300

    Co-authored-by: Lucas Augusto <ropasdef@users.noreply.github.com>

--- encriptografar.py
+++ encriptografar.py
@@ -1,6 +1,9 @@
 # Encriptografar
 
 import math
+import random
+import numpy as np
+
 
 print ('Encriptografar\n ')
 print ('\n Insira p:')
@@ -20,7 +23,7 @@ print ('\n phi = (p - 1) * (q - 1) =', phi)
 
 
 
-# Calcula D
+# Acha todos os valores de D
 i = 2
 valores_d = []
 while (i < phi):
@@ -29,7 +32,52 @@ while (i < phi):
         valores_d.append(i)
     i = i + 1
 
-# imprimir todos os valores de d
-print ('\n Valores de d:')
-for i in valores_d:
-    print (i)
+# Determina um valor de d aleatório
+d = random.choice(valores_d)
+
+print ('\n d =', d)
+
+# Calcula e
+#(E * D) mod Z = 1 (Z = phi)
+e = 0
+valores_e = []
+while (e < phi):
+    if ((e * d) % phi == 1):
+        valores_e.append(e)
+    e = e + 1
+# Determina um valor de e aleatório
+e = random.choice(valores_e)
+    
+print ('\n e =', e)
+
+# Calcula a chave privada
+chave_privada = (n, d)
+print ('\n Chave privada:', chave_privada)
+
+# Calcula a chave pública
+chave_publica = (n, e)
+print ('\n Chave pública:', chave_publica)
+
+# Insere texto a ser encriptografado
+print ('\n Insira a mensagem:')
+mensagem = input()
+
+# Encriptografar pela tabela ASCII
+mensagem_encriptografada = []
+for letra in mensagem:
+    mensagem_encriptografada.append((ord(letra) ** e )% n)
+
+# Convertendo para string
+mensagem_criptografada = []
+for letra in mensagem_encriptografada:
+    mensagem_criptografada.append(chr(letra))
+    
+print ('\n Mensagem desencriptografada:', "".join(mensagem_criptografada))
+
+# Desencriptografar
+mensagem_desencriptografada = []
+for letra in mensagem_encriptografada:
+    mensagem_desencriptografada.append(chr((letra ** d) % n))
+print ('\n Mensagem desencriptografada:', "".join(mensagem_desencriptografada))
+
+
